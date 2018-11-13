5b8acbce2cc2a9336e6b3daea8eaade4b7c551f1
융미 <yoonmee@yungmiui-MacBook-Pro.local>
2018-11-13 23:36:10 +0900

    change directory

---
+++ node_modules/react-typist/src/Backspace.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Backspace = () => <noscript />;
+
+Backspace.propTypes = {
+  count: PropTypes.number,
+  delay: PropTypes.number,
+};
+
+Backspace.defaultProps = {
+  count: 1,
+  delay: 0,
+};
+
+export default Backspace;
